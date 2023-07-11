0aa8fb5a7f4467d165d3208005724978934e78f0
Krishak15 <anandevu2@gmail.com>
2023-07-11 13:15:40 +0530

    Added hero transition from home page, Fixed bg image select bug, Enhanced UI

--- lib/views/note_editor.dart
+++ lib/views/note_editor.dart
@@ -11,7 +11,10 @@ import 'package:provider/provider.dart';
 import '../widgets/color_picker.dart';
 
 class NoteEditorPage extends StatefulWidget {
-  const NoteEditorPage({super.key});
+  final id;
+
+  final wholeData;
+  const NoteEditorPage({super.key, required this.id, required this.wholeData});
 
   @override
   State<NoteEditorPage> createState() => _NoteEditorPageState();
@@ -26,7 +29,7 @@ class _NoteEditorPageState extends State<NoteEditorPage> {
 
   int? colorID;
   int? selectedColorIndex = 1;
-  int? selectedImageIndex;
+  int? selectedImageIndex = 1;
 
   // String date = DateTime.now().toString();
 
@@ -39,176 +42,198 @@ class _NoteEditorPageState extends State<NoteEditorPage> {
 
   @override
   Widget build(BuildContext context) {
+    //initial text to edit
+    _titleController.text = widget.wholeData['title'];
+    _contentController.text = widget.wholeData['description'];
+
     AppStyle colorProvider = Provider.of<AppStyle>(context);
     colorID = Random().nextInt(colorProvider.cardsColor.length);
+
+    void resetIndex() {
+      colorProvider.setSelectedImageIndex(1);
+    }
 
     selectedColorIndex = colorProvider.selectedColorIndex;
     selectedImageIndex = colorProvider.selectedImageIndex;
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: Text("Add Note"),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-      ),
-      backgroundColor: colorProvider.cardsColor[selectedColorIndex ?? colorID!],
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image:
-                  AssetImage(colorProvider.bgImagesP[selectedImageIndex ?? 1]),
-              fit: BoxFit.cover),
+    final snapShotData = widget.wholeData;
+
+    return WillPopScope(
+      onWillPop: () async {
+        resetIndex();
+        return true;
+      },
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: Text("Add Note"),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Form(
+        backgroundColor:
+            colorProvider.cardsColor[selectedColorIndex ?? colorID!],
+        body: Hero(
+          tag: 'card${widget.id}',
+          child: Material(
+            color: Colors.transparent,
+            shadowColor: Colors.transparent,
+            child: Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(selectedImageIndex == 1
+                        ? colorProvider.bgImagesP[snapShotData['image_id']]
+                        : colorProvider.bgImagesP[selectedImageIndex!]),
+                    fit: BoxFit.cover),
+              ),
+              child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    const SizedBox(
-                      height: 100,
-                    ),
-                    Text(getFormattedDateTime()),
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: TextFormField(
-                        controller: _titleController,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Note Title',
-                        ),
-                        onChanged: (value) {
-                          title = value;
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: TextFormField(
-                        controller: _contentController,
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Note Content',
-                        ),
-                        onChanged: (value) {
-                          description = value;
-                        },
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Row(
-                          children: [
-                            InkWell(
-                              onTap: () {
-                                showModalBottomSheet(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return Column(
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: SizedBox(
-                                                height: 200,
-                                                child: ColorPickerWidget()),
-                                          ),
-                                        ],
-                                      );
-                                    });
+                    Form(
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 100,
+                          ),
+                          Text(getFormattedDateTime()),
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: TextFormField(
+                              controller: _titleController,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Note Title',
+                              ),
+                              onChanged: (value) {
+                                title = value;
                               },
-                              child: const Icon(
-                                Icons.color_lens,
-                                size: 30,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: TextFormField(
+                              controller: _contentController,
+                              keyboardType: TextInputType.multiline,
+                              maxLines: null,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Note Content',
+                              ),
+                              onChanged: (value) {
+                                description = value;
+                              },
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Row(
+                                children: [
+                                  InkWell(
+                                    onTap: () {
+                                      showModalBottomSheet(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return Column(
+                                              children: [
+                                                Padding(
+                                                  padding:
+                                                      const EdgeInsets.all(8.0),
+                                                  child: SizedBox(
+                                                      height: 200,
+                                                      child:
+                                                          ColorPickerWidget()),
+                                                ),
+                                              ],
+                                            );
+                                          });
+                                    },
+                                    child: const Icon(
+                                      Icons.color_lens,
+                                      size: 30,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  InkWell(
+                                    onTap: () {
+                                      showModalBottomSheet(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return const Column(
+                                              children: [
+                                                Padding(
+                                                  padding: EdgeInsets.all(8.0),
+                                                  child: SizedBox(
+                                                      height: 300,
+                                                      child: BgPickerWidget()),
+                                                ),
+                                              ],
+                                            );
+                                          });
+                                    },
+                                    child: const Icon(
+                                      Icons.image,
+                                      size: 30,
+                                    ),
+                                  )
+                                ],
                               ),
                             ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            InkWell(
-                              onTap: () {
-                                showModalBottomSheet(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return const Column(
-                                        children: [
-                                          Padding(
-                                            padding: EdgeInsets.all(8.0),
-                                            child: SizedBox(
-                                                height: 300,
-                                                child: BgPickerWidget()),
-                                          ),
-                                        ],
-                                      );
-                                    });
-                              },
-                              child: const Icon(
-                                Icons.image,
-                                size: 30,
-                              ),
-                            )
-                          ],
-                        ),
+                          )
+                        ],
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
-            ],
+            ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () async {
-          if (_titleController.text.isNotEmpty &&
-              _contentController.text.isNotEmpty) {
-            addData();
-          } else {
-            const snackBar = SnackBar(
-              content: Text('Add Something !'),
-            );
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () async {
+            if (_titleController.text.isNotEmpty &&
+                _contentController.text.isNotEmpty) {
+              updateData();
+              colorProvider.setSelectedImageIndex(1);
+            } else {
+              const snackBar = SnackBar(
+                content: Text('Add Something !'),
+              );
 
-            // Find the ScaffoldMessenger in the widget tree
-            // and use it to show a SnackBar.
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          }
-          // FirebaseFirestore.instance.collection("Notes").add({
-          //   "note_title": _titleController.text,
-          //   "note_content": _contentController.text,
-          //   "creation_date": date,
-          //   "color_id": colorID,
-          // }).then((value) {
-          //   Navigator.pop(context);
-          // }).catchError((error) => print('Failed to add note: $error'));
-        },
-        label: Text('Save'),
-        icon: Icon(Icons.save_rounded),
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }
+          },
+          label: Text('Save'),
+          icon: Icon(Icons.save_rounded),
+        ),
       ),
     );
   }
 
-  void addData() {
+  void updateData() {
     CollectionReference ref = FirebaseFirestore.instance
         .collection("users")
         .doc(FirebaseAuth.instance.currentUser!.uid)
         .collection('notes');
 
+    DocumentReference docRef = ref.doc(widget.id);
+
     var data = {
-      "title": title,
-      "description": description,
+      "title": title ?? _titleController.text,
+      "description": description ?? _contentController.text,
       "color_id": selectedColorIndex,
       "image_id": selectedImageIndex,
       "created": getFormattedDateTime()
     };
-    ref.add(data);
+    docRef.update(data);
 
     //
+    selectedImageIndex = 1;
+    print(selectedImageIndex);
     //
     Navigator.pop(context);
   }
