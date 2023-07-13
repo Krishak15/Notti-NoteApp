a26664be716b9a146c92710fc693e095cfc6a490
Krishak15 <anandevu2@gmail.com>
2023-07-13 18:52:24 +0530

    save data of account's pic,name and email

--- lib/controller/google_auth.dart
+++ lib/controller/google_auth.dart
@@ -1,12 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_noteapp/views/home_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 GoogleSignIn googleSignIn = GoogleSignIn();
 final FirebaseAuth auth = FirebaseAuth.instance;
 CollectionReference users = FirebaseFirestore.instance.collection("users");
+
+void saveString(String value, String gmailVal, String dispName) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setString('photo', value);
+
+  prefs.setString('gmail', gmailVal);
+
+  prefs.setString('displayName', dispName);
+}
 
 ///
 //Auth Function
@@ -49,12 +60,20 @@ Future<bool?> signInWithGoogle(BuildContext context) async {
 
       users.doc(user.uid).get().then((doc) {
         if (doc.exists) {
+          saveString(
+              googleSignInAccount.photoUrl!,
+              googleSignInAccount.email.toString(),
+              googleSignInAccount.displayName.toString());
           //if user already exists
           doc.reference.update(userData);
           Navigator.of(context).push(MaterialPageRoute(
             builder: (context) => const HomeScreen(),
           ));
         } else {
+          // saveString(
+          //     googleSignInAccount.photoUrl!,
+          //     googleSignInAccount.email.toString(),
+          //     googleSignInAccount.displayName.toString());
           users.doc(user.uid).set(
               userData); //if there's no existing user with this email address
           Navigator.of(context).push(MaterialPageRoute(
@@ -64,7 +83,12 @@ Future<bool?> signInWithGoogle(BuildContext context) async {
       });
     }
   } catch (PlatformExeception) {
-    print(PlatformExeception);
-    print("SignIn not succesful");
+    if (kDebugMode) {
+      print(PlatformExeception);
+    }
+    if (kDebugMode) {
+      print("SignIn not succesful");
+    }
   }
+  return null;
 }
