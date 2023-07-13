add92a8ff1f2d6fb204115cb29bf10b2c7ebd2e1
Krishak15 <anandevu2@gmail.com>
2023-07-13 18:55:41 +0530

    fixed bg picker

--- lib/widgets/bg_picker.dart
+++ lib/widgets/bg_picker.dart
@@ -15,9 +15,9 @@ class _BgPickerWidgetState extends State<BgPickerWidget> {
   Widget build(BuildContext context) {
     AppStyle colorProvider = Provider.of<AppStyle>(context);
     return GridView.builder(
-      scrollDirection: Axis.horizontal,
+      scrollDirection: Axis.vertical,
       gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
       itemCount: colorProvider.bgImagesP.length,
       itemBuilder: (context, index) {
         var img = colorProvider.bgImagesP[index];
@@ -32,32 +32,45 @@ class _BgPickerWidgetState extends State<BgPickerWidget> {
               // Navigator.pop(context);
             });
           },
-          child: Container(
-            margin: const EdgeInsets.all(8),
-            width: 160,
-            height: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage(img),
+          child: Stack(
+            children: [
+              isFirst == 0
+                  ? const Center(
+                      child: Icon(
+                        Icons.undo,
+                        color: Colors.blueGrey,
+                        size: 40,
+                      ),
+                    )
+                  : const SizedBox(),
+              Container(
+                margin: const EdgeInsets.all(8),
+                width: 160,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(img),
+                  ),
+                  border: Border.all(
+                    color: isSelected ? Colors.black : Colors.transparent,
+                    width: isSelected ? 2 : 0,
+                  ),
+                ),
+                child: isFirst == 1
+                    ? const SizedBox(
+                        height: 70,
+                        width: 70,
+                        child: Icon(
+                          Icons.hide_image,
+                          color: Colors.blueGrey,
+                          size: 40,
+                        ),
+                      )
+                    : null,
               ),
-              border: Border.all(
-                color: isSelected ? Colors.black : Colors.transparent,
-                width: isSelected ? 2 : 0,
-              ),
-            ),
-            child: isFirst == 0
-                ? const SizedBox(
-                    height: 70,
-                    width: 70,
-                    child: Icon(
-                      Icons.hide_image,
-                      color: Colors.blueGrey,
-                      size: 40,
-                    ),
-                  )
-                : null,
+            ],
           ),
         );
       },
